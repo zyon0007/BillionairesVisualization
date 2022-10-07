ca112dff69dc08f11f735d2c213ea73ebfe74e7d
zyon0007 <111486025+zyon0007@users.noreply.github.com>
2022-10-08 00:11:39 +0800

    Add files via upload

---
+++ barchart.js
@@ -0,0 +1,4 @@
+var vg_1 = "BarChart.json";
+vegaEmbed("#barchart", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
