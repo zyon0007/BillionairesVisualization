6df12241104320a04d4831c366d7880c1740e77c
zyon0007 <111486025+zyon0007@users.noreply.github.com>
2022-10-08 00:23:21 +0800

    Update USA_Billionaires_map.js

--- USA_Billionaires_map.js
+++ USA_Billionaires_map.js
@@ -1,4 +1,4 @@
-var spec2 = "USA_Billionaires.json";
+var spec2 = "Map.json";
   vegaEmbed('#choropleth_map', spec2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
